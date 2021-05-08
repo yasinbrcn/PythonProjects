dfa6882b8b3fe541a0b6bfc3de3b0784e08bb291
Yasin BİRCAN <yasinbrcn@gmail.com>
2021-05-08 20:46:53 +0300

    [*]: Scoreboard has been added to the code of the third project.

--- Project_3 - Rock,Paper,Scissors/rock_paper_scissors.py
+++ Project_3 - Rock,Paper,Scissors/rock_paper_scissors.py
@@ -4,40 +4,68 @@ Date:     08.05.2021
 '''
 import random
 import keyboard
+user_score = 0
+pc_score = 0
 
-print("Please select --> [1]:Rock , [2]:Paper , [3]:Scissors")
-if keyboard.read_key() == "1":
-    user_choice = "Rock"
-    #print("You pressed 1")
-elif keyboard.read_key() == "2":
-    user_choice = "Paper"
-    #print("You pressed 2")
-elif keyboard.on_press_key("3", lambda _:print("You pressed 3")):
-    user_choice = "Scissors"
-    #print("You pressed 3")
-print("You Choice: " + user_choice)
+for i in range(5):
 
-pc_choice_list=["Rock","Paper","Scissors"]
-pc_choice = random.choice(pc_choice_list)
-print("PC Choice: " + pc_choice)
-if user_choice == pc_choice:
-    print("Scoreless")
-elif user_choice == "Rock" and pc_choice == "Paper":
-    print("PC Win")
-elif user_choice == "Rock" and pc_choice == "Scissors":
-    print("You Win")
-elif user_choice == "Paper" and pc_choice == "Rock":
-    print("You Win")
-elif user_choice == "Paper" and pc_choice == "Scissors":
-    print("PC Win")
-elif user_choice == "Scissors" and pc_choice == "Rock":
-    print("PC Win")
-elif user_choice == "Scissors" and pc_choice == "Paper":
-    print("You Win")
+    user_choice = str(input("Please select --> [1]:Rock , [2]:Paper , [3]:Scissors"))
+    if user_choice == "1":
+        user_choice = "Rock"
+    elif user_choice == "2":
+        user_choice = "Paper"
+    elif user_choice == "3":
+        user_choice = "Scissors"
+    print("You Choice: " + user_choice)
 
-
-
-   
-    
-            
-
+    pc_choice_list=["Rock","Paper","Scissors"]
+    pc_choice = random.choice(pc_choice_list)
+    print("PC Choice: " + pc_choice)
+    if user_choice == pc_choice:
+        print("Scoreless")
+        user_score = user_score
+        pc_score = pc_score
+        print("You Score: " + str(user_score))
+        print("PC Score: " + str(pc_score))
+    elif user_choice == "Rock" and pc_choice == "Paper":
+        print("PC Win")
+        user_score = user_score
+        pc_score = pc_score + 1
+        print("You Score: " + str(user_score))
+        print("PC Score: " + str(pc_score))
+    elif user_choice == "Rock" and pc_choice == "Scissors":
+        print("You Win")
+        user_score = user_score+1
+        pc_score = pc_score
+        print("You Score: " + str(user_score))
+        print("PC Score: " + str(pc_score))
+    elif user_choice == "Paper" and pc_choice == "Rock":
+        print("You Win")
+        user_score = user_score+1
+        pc_score = pc_score
+        print("You Score: " + str(user_score))
+        print("PC Score: " + str(pc_score))
+    elif user_choice == "Paper" and pc_choice == "Scissors":
+        print("PC Win")
+        user_score = user_score
+        pc_score = pc_score+1
+        print("You Score: " + str(user_score))
+        print("PC Score: " + str(pc_score))
+    elif user_choice == "Scissors" and pc_choice == "Rock":
+        print("PC Win")
+        user_score = user_score
+        pc_score = pc_score+1
+        print("You Score: " + str(user_score))
+        print("PC Score: " + str(pc_score))
+    elif user_choice == "Scissors" and pc_choice == "Paper":
+        print("You Win")
+        user_score = user_score+1
+        pc_score = pc_score
+        print("You Score: " + str(user_score))
+        print("PC Score: " + str(pc_score))
+if user_score == pc_score:
+    print("Final: Scoreless!")
+elif user_score > pc_score:
+    print("Final: You Win!")
+elif user_score < pc_score:
+    print("Final: PC Win!")
