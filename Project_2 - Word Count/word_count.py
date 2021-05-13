e9f8abe4921c1b20b6b90af6f8dbe0a4f877e866
Yasin BİRCAN <yasinbrcn@gmail.com>
2021-05-14 01:54:28 +0300

    [*]: Control command has been added for user to enter only string value.

--- Project_2 - Word Count/word_count.py
+++ Project_2 - Word Count/word_count.py
@@ -2,12 +2,23 @@
 Coded By: Yasin BİRCAN
 Date:     08.05.2021
 '''
-word = str(input("Please Enter The Word:"))
-word=word.strip()
-wordlist=[]
-j=1
-for i in range(len(word)):
-    wordlist.insert(i,word[i:j])
-    j=j+1
+word = input("Please enter the sentence:")
+try:
+    word = int(word)
+    print("Please do not enter number.")
+except:
+    try:
+        word = float(word)
+        print("Please do not enter number.")
+    except:
+        word=word.strip()
+        wordlist=[]
+        j=1
+        for i in range(len(word)):
+            wordlist.insert(i,word[i:j])
+            j=j+1
 print("Character Count: " + str(len(word) - wordlist.count(" ")))
-print("Word Count: " + str(wordlist.count(" ")+1))
+if wordlist.count(" ") == 0:
+    print("Word Count: " + str(wordlist.count(" ")))
+else:
+    print("Word Count: " + str(wordlist.count(" ")+1))
