6695e03f8fa16fc8fa06fccdf3010fb2fb6d9d4d
Yasin BİRCAN <yasinbrcn@gmail.com>
2021-05-14 01:13:31 +0300

    [*]: The "int", "str" and "float" controls have been added to the codes.

--- Project_1 - Odd or Even Number/odd_or_even_number.py
+++ Project_1 - Odd or Even Number/odd_or_even_number.py
@@ -2,8 +2,19 @@
 Coded By: Yasin BİRCAN
 Date:     08.05.2021
 '''
-number = int(input("Please enter a number:"))
-if(number%2 == 0):
-    print("Number is a even...")
-else:
-    print("Number is a odd...")
+key=1
+while key == 1:
+    number = input("Please enter a number:")
+    try:
+        number = int(number)
+        if(number%2 == 0):
+            print("Number is a even.")
+        else:
+            print("Number is a odd.")
+        key=0
+    except:
+        try:
+            number = float(number)
+            print("Please enter a integer number.")
+        except:
+            print("Please enter number only.")
