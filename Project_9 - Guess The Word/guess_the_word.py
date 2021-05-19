87361225f20c3255a11899ee4677d9160d90f132
Yasin BİRCAN <yasinbrcn@gmail.com>
2021-05-19 13:31:31 +0300

    [*]: While loop has been added for guess.

--- Project_9 - Guess The Word/guess_the_word.py
+++ Project_9 - Guess The Word/guess_the_word.py
@@ -1,5 +1,8 @@
 import random
 j=1
+key=1
+guess_4 = 3
+guess_5 = 4
 chosen_word_list = []
 find_letter_list = []
 line_list_4 = ["_ ","_ ","_ ","_ "]
@@ -14,53 +17,49 @@ def listString(s):
 word_list = ["able","baby","city","heart","world","angel"]
 chosen_word = random.choice(word_list)
 chosen_word_lenght = len(chosen_word)
-#chosen_word = "baby"
-#for i in range(len(chosen_word)):
-#    chosen_word_list.insert(i,chosen_word[i:j])
-#    j=j+1
-    
-user_letter = input("Please enter a letter: ")
 
-print(chosen_word)
-#print(chosen_word_list)
-#print(chosen_word_list.index(user_letter))
-'''
-for k in range(chosen_word_lenght):
-    if chosen_word[k] == user_letter and chosen_word_lenght == 4:
-        #print(str(k)+ ". eleman " + user_letter + " a eşittir.")
-        line_list_4[k] = user_letter
-        #print(listString(line_list_4))
-    elif chosen_word[k] == user_letter and chosen_word_lenght == 5:
-        line_list_5[k] = user_letter
-        #print(listString(line_list_5))
-    else:
-        print(listString(line_list_4))
-    
-
-#print(line_list.index("b"))
-'''
-'''
-for k in range(chosen_word_lenght):
-    if chosen_word[k] == user_letter:
-        find_letter_list.append(k)
-    else:
-        find_letter_list = find_letter_list
-
-print(find_letter_list)
-print(len(find_letter_list))
-
-'''
-        
 if chosen_word_lenght == 4:
-    for k in range(chosen_word_lenght):
-        if chosen_word[k] == user_letter:
-            line_list_4[k] = user_letter
+    print("You have four guesses.")
+    line_list_4[0] = chosen_word[0]
     print(listString(line_list_4))
-
-elif chosen_word_lenght == 5:
-    for k in range(chosen_word_lenght):
-        if chosen_word[k] == user_letter:
-            line_list_5[k] = user_letter
+else:
+    print("You have five guesses.")
+    line_list_5[0] = chosen_word[0]
     print(listString(line_list_5))
 
+while key == 1:
+    user_letter = input("Please enter a letter: ")
     
+    if chosen_word_lenght == 4:
+        
+        for k in range(chosen_word_lenght):
+            if chosen_word[k] == user_letter:
+                line_list_4[k] = user_letter
+                
+        print(listString(line_list_4))
+        if guess_4 != 0 and line_list_4.count("_ ") != 0:
+            guess_4 = guess_4 - 1
+            key = 1
+        elif line_list_4.count("_ ") == 0:
+            print("Correct...")
+            key = 0
+        elif guess_4 == 0:
+            print("Word is: " + chosen_word)
+            key = 0
+        
+    elif chosen_word_lenght == 5:
+
+        for k in range(chosen_word_lenght):
+            if chosen_word[k] == user_letter:
+                line_list_5[k] = user_letter
+                
+        print(listString(line_list_5))
+        if guess_5 != 0 and line_list_5.count("_ ") != 0:
+            guess_5 = guess_5 - 1
+            key = 1
+        elif line_list_5.count("_ ") == 0:
+            print("Correct...")
+            key = 0
+        elif guess_5 == 0:
+            print("Word is: " + chosen_word)
+            key = 0
